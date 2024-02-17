4111a14cb719258a9332bd08a8404e3499217dcb
BILIAMINU SALAMI <basalami2016@gmail.com>
2024-02-17 14:40:05 -0500

    added new project to oss-sec repo

---
+++ src/main/java/io/oss/osssec/OssSecApplication.java
@@ -0,0 +1,13 @@
+package io.oss.osssec;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OssSecApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OssSecApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/oss/osssec/OssSecApplicationTests.java
@@ -0,0 +1,13 @@
+package io.oss.osssec;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OssSecApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
